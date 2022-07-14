842930597bd49d32d62bb46f9640f42a29a3cb45
Verma Ajitesh (uidv5394) <Ajitesh.Verma@continental-corporation.com>
2022-07-14 14:27:53 +0200

    1. stack overflow api added 2. division on the basis of lang is done

--- api_testing/api.py
+++ api_testing/api.py
@@ -14,7 +14,7 @@ def parse_a_url(url):
 
     # response = requests.post(url, json=data)
 
-    response = requests.get(url)
+    response = requests.get(url, verify=False)
 
     ok_to_use = check_status_code(response)
     if ok_to_use:
@@ -22,6 +22,14 @@ def parse_a_url(url):
         jsonResponse = response.json()
         print("Entire JSON response")
         print(json.dumps(jsonResponse, sort_keys=True, indent=4))
+
+        # print golang questions
+        for item in jsonResponse["items"]:
+            lang = item["tags"]
+            ques = item["title"]
+            if "go" or "python" in lang:
+                print(f'languages used: {lang} and the question is {ques}:')
+
 
 
 def check_status_code(resposnse):
@@ -38,7 +46,8 @@ def check_status_code(resposnse):
 
 if __name__ == '__main__':
     # main function starts
-    url = "https://owen-wilson-wow-api.herokuapp.com/wows/movies"
+    url = "https://api.stackexchange.com/2.3/questions?fromdate=1656633600&order=desc&sort=activity&site=stackoverflow"
+    #url = "https://randomfox.ca/floof"
     try:
         parse_a_url(url)
     except HTTPError as httperr:
